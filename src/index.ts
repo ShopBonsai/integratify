66a33263ec573e7fafd88c42dd9907213205050f
Brent Van Geertruy <brentvangeertruy@outlook.com>
2019-11-19 15:23:42 -0500

    Optional options

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,18 @@ export {
 
 export const route = {
   get: (path: string) => ({
-    send: (opts: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
   }),
   post: (path: string) => ({
-    send: (opts: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
   }),
   put: (path: string) => ({
-    send: (opts: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
   }),
   patch: (path: string) => ({
-    send: (opts: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
   }),
   delete: (path: string) => ({
-    send: (opts: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
   }),
 };

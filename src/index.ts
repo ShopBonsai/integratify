6006ab0cd194ec90d8946f3746a8e9f587755a80
Brent Van Geertruy <brentvangeertruy@outlook.com>
2019-11-19 15:31:13 -0500

    Typo in request types

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,15 @@ export const route = {
     send: (opts?: ISendOptions) => getRequest('GET', path, opts),
   }),
   post: (path: string) => ({
-    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('POST', path, opts),
   }),
   put: (path: string) => ({
-    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('PUT', path, opts),
   }),
   patch: (path: string) => ({
-    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('PATCH', path, opts),
   }),
   delete: (path: string) => ({
-    send: (opts?: ISendOptions) => getRequest('GET', path, opts),
+    send: (opts?: ISendOptions) => getRequest('DELETE', path, opts),
   }),
 };

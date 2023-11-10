953bf4569f31edc18e6ff9ecbd5e4946e0294a78
Brent Van Geertruy <brentvangeertruy@outlook.com>
2023-11-09 22:14:25 -0700

    ⬆️ v6.0.0 - Drop Node 14 & 16 support (#26)
    * Upgrade yarn, TS and set Node version to min 18
    
    * Update README
    
    * Update CI
    
    * Swap npm run with yarn commands

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { integratify } from './integratify';
-
 export * from './common/interfaces';
 export { NUM_ERROR_CHECKS } from './utils/expect';
 
 export { setGlobalConfig as setConfiguration } from './config/global';
 
 // Export named & default main integratify function
-export { integratify };
-export default integratify;
+
+export { integratify, integratify as default } from './integratify';
